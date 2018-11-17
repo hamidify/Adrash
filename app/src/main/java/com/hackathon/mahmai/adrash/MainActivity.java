f9d4c015af5102f57b2a2400043a148e157070e2
Abdulahamid Abdo <abdoabdulhamid@gmail.com>
2018-11-17 22:14:54 +0300

    Added A nav

--- app/src/main/java/com/hackathon/mahmai/adrash/MainActivity.java
+++ app/src/main/java/com/hackathon/mahmai/adrash/MainActivity.java
@@ -1,13 +1,42 @@
 package com.hackathon.mahmai.adrash;
 
+import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private NavigationView navigationView;
+    private ActionBarDrawerToggle drawerToggle;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = findViewById(R.id.toolbar);
+        navigationView = findViewById(R.id.navigation_view);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        setSupportActionBar(toolbar);
+//        toolbar.setNavigationIcon(R.drawable.nav);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.app_name, R.string.app_name);
+            drawerToggle.setDrawerIndicatorEnabled(true);
+            drawerLayout.setDrawerListener(drawerToggle);
+            drawerToggle.syncState();
+        }
+    }
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        //To change the hamburger icon to back arrow and vice versa
+        drawerToggle.syncState();
     }
 }
