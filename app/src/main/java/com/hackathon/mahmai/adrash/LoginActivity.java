96a66174c14f73cd466904c692331745a36a1956
Abdulahamid Abdo <abdoabdulhamid@gmail.com>
2018-11-17 20:35:03 +0300

    Removeed toast

--- app/src/main/java/com/hackathon/mahmai/adrash/LoginActivity.java
+++ app/src/main/java/com/hackathon/mahmai/adrash/LoginActivity.java
@@ -62,8 +62,5 @@ public class LoginActivity extends AppCompatActivity {
                 Toast.makeText(LoginActivity.this, "Something went wrong.", Toast.LENGTH_SHORT).show();
             }
         });
-        Toast.makeText(this, "Successfuly Loged In.", Toast.LENGTH_SHORT).show();
-
-
     }
 }
